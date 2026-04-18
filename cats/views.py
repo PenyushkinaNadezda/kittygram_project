5e6b23c84871b2fac0cda5d8d21bd23aeb0c311c
Penyushkina Nadezda <najia00@qq.com>
2026-04-18 16:59:31 +0800

    kittygram

--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Cat
@@ -21,17 +20,29 @@ class APICat(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PATCH', 'PUT', 'DELETE'])
-def cat_detail(request, pk):
-    cat = Cat.objects.get(id=pk)
-    if request.method == 'PATCH' or request.method == 'PUT':
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        cat = Cat.objects.get(id=pk)
         serializer = CatSerializer(cat, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def put(self, request, pk):
+        cat = Cat.objects.get(id=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cat = Cat.objects.get(id=pk)
         cat.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    serializer = CatSerializer(cat)
-    return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cats.views import APICat, cat_detail
+from cats.views import APICat, APICatDetail
 
 urlpatterns = [
    path('cats/', APICat.as_view()),
-   path('cats/<int:pk>/', cat_detail)
+   path('cats/<int:pk>/', APICatDetail.as_view())
 ]
